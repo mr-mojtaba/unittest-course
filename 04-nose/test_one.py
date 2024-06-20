85c7fa93d74b1dc0227e3f5a82b2937540d4e952
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-06-20 12:56:01 +0330

    The lesson related to nose.

---
+++ 04-nose/test_one.py
@@ -0,0 +1,27 @@
+# Need to install nose ( pip install nose ) (Requires Python 3.7 or lower)
+import one
+
+
+def test_add():
+    assert one.add(3, 4) == 7
+    assert one.add(-1, 4) == 3
+    assert one.add(0, 8) == 8
+
+
+def test_subtract():
+    assert one.subtract(4, 5) == -1
+    assert one.subtract(9, 0) == 9
+
+
+def test_multiply():
+    assert one.multiply(3, 4) != 11
+
+
+def test_division():
+    assert one.division(50, 5) == 10
+
+
+# اجرا در ترمینال برای اجرای تست
+# nosetests test_one.py
+# اجرا در ترمینال برای اجرای تست با جزئیات
+# nosetests -v test_one.py
